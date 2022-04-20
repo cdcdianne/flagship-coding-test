50ce8c417cd2708cb5b489cca50ad027c08c3e15
Dayana <cdcdianne@gmail.com>
2022-04-20 18:06:23 +0800

    Product - Add to cart Submit functionality

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,47 @@
 // Put your application javascript here
+
+$( function () {
+
+
+    // START:: Product - Add to cart Submit button
+    var frm = $('#AddToCartForm');
+
+    frm.submit(function (e) {
+        e.preventDefault();
+        let cartIndex = 0;
+        $.get("/cart.js", null, (data) => {
+            const totalItems = data?.items?.length;
+
+            if(totalItems){
+                cartIndex=totalItems
+            }
+
+            const formData = {
+                items: [
+                    {
+                        id: $('#productSelect').val(),
+                        quantity: $('#Quantity').val(),
+                        properties: {
+                            cart_index: cartIndex,
+                        }
+    
+                    }
+    
+                ]
+            }
+
+            $.ajax({
+                type: frm.attr('method'),
+                url: frm.attr('action'),
+                data: formData,
+                success: function (data) {
+                    console.log('Submission was successful.');
+                    window.location.href="/cart"
+                },
+                error: function (data) {
+                    console.log('An error occurred.');
+                },
+            });
+        }, "json" )
+    });
+    // END:: Product - Add to Cart Submit Button
